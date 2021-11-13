e20de61a5f681fdf5f846b8da6a56a80c1c418bf
Constantine <tift@MacBook-Air-Konstantin.local>
2021-11-13 17:50:38 +1000

    Add first test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,1 +1,20 @@
 #include "gtest/gtest.h"
+
+#include "stack.h"
+
+TEST(StackTest, StackPush)
+{
+    Stack *stack = stack_create();
+
+    stack_push(stack, 11);
+    EXPECT_EQ(11, stack->head->value);
+
+    stack_push(stack, 12);
+    EXPECT_EQ(12, stack->head->value);
+    EXPECT_EQ(11, stack->head->next->value);
+
+    stack_push(stack, 13);
+    EXPECT_EQ(13, stack->head->value);
+    EXPECT_EQ(12, stack->head->next->value);
+    EXPECT_EQ(11, stack->head->next->next->value);
+}
